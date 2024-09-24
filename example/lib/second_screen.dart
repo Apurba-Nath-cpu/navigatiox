a664757d5688ed8953e6a6eb2a0cc0df7bfe5f35
Apurba-Nath-cpu <apurba64880@gmail.com>
2024-09-24 16:49:21 +0530

    Initial commit

--- example/lib/second_screen.dart
+++ example/lib/second_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({
-    super.key
-  });
+  const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
